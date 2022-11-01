eb62f0c65aa386f8c3802a657d3083edf7ec8484
JeremyXZ <jeremyuk168@gmail.com>
2022-11-01 21:07:21 +0000

    calculateScrabbleScore funciton pass all tests

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,10 +1,35 @@
-export function calculateScrabbleScore(word) {
-    const scoreOneArr = ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"]
+const scoreOneArr = ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"]
+const scoreTwoArr = ["D", "G"];
+const scoreThreeArr = ["B", "C", "M", "P"];
+const scoreFourArr = ["F", "H", "V", "W", "Y"];
+const scoreFiveArr = ["K"];
+const scoreEightArr = ["J", "X"];
+const scoreTenArr = ["Q", "Z"];
+
+function calculateScrabbleScore(word) {
+   
     if (scoreOneArr.includes (word)) {
         return 1
     } else if (word === "K"){
-        
-            return 5
-        } else if (word === "D" || word === "G") {
-            return 2}
-        }
+        return 5
+    } else if (word === "D" || word === "G") {
+        return 2
+    } else if(scoreThreeArr.includes(word)) {
+        return 3
+    } else if(scoreFourArr.includes(word)) {
+        return 4
+    } else if(scoreEightArr.includes(word)) {
+        return 8
+    } else if(scoreTenArr.includes(word)) {
+        return 10
+    }
+}
+export {
+    scoreOneArr,
+    scoreThreeArr,
+    scoreFourArr,
+    scoreFiveArr,
+    scoreEightArr,
+    scoreTenArr,
+    calculateScrabbleScore
+}
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,49 +1,81 @@
 
-import { calculateScrabbleScore } from "./scrabble-score";
-describe ("scrabble test suite'", () => {
-    const scoreOneArr = ["A", "E", "I", "L", "N", "O", "R", "S", "T", "U"]
+import { 
+    scoreOneArr,
+    scoreThreeArr,
+    scoreFourArr,
+    scoreFiveArr,
+    scoreEightArr,
+    scoreTenArr,
+    calculateScrabbleScore
+} from "./scrabble-score";
+
+
+describe ("calculateScrabbleScore function", () => {
+
     
-    const scoreTwoArr = ["D", "G"];
-    const scoreThreeArr = ["B", "C", "M", "P"];
-    const scoreFourArr = ["F", "H", "V", "W", "Y"];
-    const scoreFiveArr = ["K"];
-    const scoreEightArr = ["J", "X"];
-    const scoreTenArr = ["Q", "Z"];
 
-
-    test(`When calculateScrabbleScore receives any letter from scoreOneArr, it returns 1`, () => {
-        let randomIndex = Math.floor(Math.random()*(scoreOneArr.length));
-        let actual = calculateScrabbleScore(scoreOneArr[randomIndex]);
-        let expected = 1
-
-        expect(actual).toBe(expected);
-
-   })
-
-
-    test(`When calculateScrabbleScore is in one of the letters in scoreFiveArr as an argument, it returns 5`, () => {
+     test(`When calculateScrabbleScore takes "K" as an argument, it returns 5`, () => {
          let actual = calculateScrabbleScore('K');
          let expected = 5
          expect(actual).toBe(expected);
 
     })
 
-    test(`When calculateScrabbleScore takes  as an argument, it returns 2`, () => {
+
+    test(`When calculateScrabbleScore takes "D"  as an argument, it returns 2`, () => {
         let actual = calculateScrabbleScore("D");
         let expected = 2
         expect(actual).toBe(expected);
 
-   })
+    })
 
-   test(`When calculateScrabbleScore takes G as an argument, it returns 2`, () => {
+   test(`When calculateScrabbleScore takes "G" as an argument, it returns 2`, () => {
     let actual = calculateScrabbleScore("G");
     let expected = 2
     expect(actual).toBe(expected);
 
-})
+    })
 
+   test(`When calculateScrabbleScore receives any letter from scoreOneArr, it returns 1`, () => {
+    let randomIndex = Math.floor(Math.random()*(scoreOneArr.length));
+    let actual = calculateScrabbleScore(scoreOneArr[randomIndex]);
+    let expected = 1
+    
+    expect(actual).toBe(expected);
 
+    })
+test(`When calculateScrabbleScore receives any letter from scoreThreeArr, it returns 3`, () => {
+    let randomIndex = Math.floor(Math.random()*(scoreThreeArr.length));
+    let actual = calculateScrabbleScore(scoreThreeArr[randomIndex]);
+    let expected = 3
+    
+    expect(actual).toBe(expected);
+    });
 
+    test(`When calculateScrabbleScore receives any letter from scoreFourArr, it returns 4`, () => {
+        let randomIndex = Math.floor(Math.random()*(scoreFourArr.length));
+        let actual = calculateScrabbleScore(scoreFourArr[randomIndex]);
+        let expected = 4
+        
+        expect(actual).toBe(expected);
+        });
+
+    test(`When calculateScrabbleScore receives any letter from scoreEightArr, it returns 8`, () => {
+        let randomIndex = Math.floor(Math.random()*(scoreEightArr.length));
+        let actual = calculateScrabbleScore(scoreEightArr[randomIndex]);
+        let expected = 8
+        
+        expect(actual).toBe(expected);
+        });
+
+    test(`When calculateScrabbleScore receives any letter from scoreTenArr, it returns 10`, () => {
+        let randomIndex = Math.floor(Math.random()*(scoreTenArr.length));
+        let actual = calculateScrabbleScore(scoreTenArr[randomIndex]);
+        let expected = 10
+        
+        expect(actual).toBe(expected);
+        });
+        
 });
 
 
